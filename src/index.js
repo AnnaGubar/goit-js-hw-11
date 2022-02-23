86e38621d1b57780ecba7ee1a65737f3973a4a99
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-02-23 21:34:55 +0200

    still need to do pagination
    and axios

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import imgCardTpl from './templates/img-card.hbs';
 import './css/styles.css';
 
+let page = 1;
 const BASE_URL = 'https://pixabay.com/api/';
 const apiKey = '25849699-edc9a69ae2fd4562ebcb7ccdf';
-const searchSettings = 'image_type=photo&orientation=horizontal&safesearch=true';
+const searchSettings = `image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
 
 const formRef = document.querySelector('#search-form');
 const galleryRef = document.querySelector('.gallery');
@@ -18,7 +19,7 @@ function searchHandler(e) {
 
   let inputValue = e.currentTarget.searchQuery.value;
 
-  fetch(`${BASE_URL}?key=${apiKey}&q=${inputValue}&${searchSettings}`)
+  fetch(`${BASE_URL}?key=${apiKey}&q=${inputValue}&${searchSettings}&page=${page}`)
     // fetch(`${BASE_URL}?key=${apiKey}&q=cat&${searchSettings}`)
     .then(res => res.json())
     .then(img => {
@@ -33,11 +34,28 @@ function searchHandler(e) {
       galleryRef.innerHTML = imgCardTpl(img.hits);
       btnLoadMoreRef.classList.remove('is-hidden');
     });
+  
+  // console.log('⭐ ~ inputValue', inputValue);
+  // loadMoreHandler(e,inputValue);
 }
 
-function loadMoreHandler(e) {
+
+function loadMoreHandler(e, xxx) {
   e.preventDefault();
-}
+  // let inputValue = e.currentTarget.searchQuery.value;
+  const x = xxx;
+  // console.log('⭐ ~ inputValue', inputValue);
+  // console.log("⭐ ~ x", x)
+  // page += 1
+  fetch(`${BASE_URL}?key=${apiKey}&q=${x}&${searchSettings}&page=2`)
+    // fetch(`${BASE_URL}?key=${apiKey}&q=cat&${searchSettings}`)
+    .then(res => res.json())
+    .then(img => {
+      console.log(img);
+      galleryRef.innerHTML = imgCardTpl(img.hits)
+    })
+    
+};
 
 function renderImgCards(img) {
   galleryRef.innerHTML = imgCardTpl(img.hits);
